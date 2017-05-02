263a5a410f7e5d688accce5d7734d0f4d9f4b9b8
Joey Brown <jbrown@viningsparks.com>
2017-05-02 16:12:17 -0500

    finished 2.3

--- src/Unit-2/DoThis/Actors/ChartingActor.cs
+++ src/Unit-2/DoThis/Actors/ChartingActor.cs
@@ -8,7 +8,20 @@ namespace ChartApp.Actors
 {
     public class ChartingActor : ReceiveActor
     {
+        public const int MaxPoints = 250;
+        private int xPosCounter = 0;
+
         #region Messages
+
+        public class RemoveSeries
+        {
+            public RemoveSeries(string seriesName)
+            {
+                SeriesName = seriesName;
+            }
+
+            public string SeriesName { get; private set; }
+        }
 
         public class InitializeChart
         {
@@ -46,43 +59,98 @@ namespace ChartApp.Actors
 
             Receive<InitializeChart>(ic => HandleInitialize(ic));
             Receive<AddSeries>(addSeries => HandleAddSeries(addSeries));
+            Receive<RemoveSeries>(removeSeries => HandleRemoveSeries(removeSeries));
+            Receive<Metric>(metric => HandleMetrics(metric));
         }
 
         #region Individual Message Type Handlers
-
-        private void HandleAddSeries(AddSeries series)
-        {
-            if (!string.IsNullOrEmpty(series.Series.Name) &&
-            !_seriesIndex.ContainsKey(series.Series.Name))
-            {
-                _seriesIndex.Add(series.Series.Name, series.Series);
-                _chart.Series.Add(series.Series);
-            }
-        }
 
         private void HandleInitialize(InitializeChart ic)
         {
             if (ic.InitialSeries != null)
             {
-                //swap the two series out
+                // swap the two series out
                 _seriesIndex = ic.InitialSeries;
             }
 
-            //delete any existing series
+            // delete any existing series
             _chart.Series.Clear();
 
-            //attempt to render the initial chart
+            // set the axes up
+            var area = _chart.ChartAreas[0];
+            area.AxisX.IntervalType = DateTimeIntervalType.Number;
+            area.AxisY.IntervalType = DateTimeIntervalType.Number;
+
+            SetChartBoundaries();
+
+            // attempt to render the initial chart
             if (_seriesIndex.Any())
             {
                 foreach (var series in _seriesIndex)
                 {
-                    //force both the chart and the internal index to use the same names
+                    // force both the chart and the internal index to use the same names
                     series.Value.Name = series.Key;
                     _chart.Series.Add(series.Value);
                 }
             }
+
+            SetChartBoundaries();
+        }
+
+        private void HandleAddSeries(AddSeries series)
+        {
+            if (!string.IsNullOrEmpty(series.Series.Name) &&
+                !_seriesIndex.ContainsKey(series.Series.Name))
+            {
+                _seriesIndex.Add(series.Series.Name, series.Series);
+                _chart.Series.Add(series.Series);
+                SetChartBoundaries();
+            }
+        }
+
+        private void HandleRemoveSeries(RemoveSeries series)
+        {
+            if (!string.IsNullOrEmpty(series.SeriesName) &&
+                _seriesIndex.ContainsKey(series.SeriesName))
+            {
+                var seriesToRemove = _seriesIndex[series.SeriesName];
+                _seriesIndex.Remove(series.SeriesName);
+                _chart.Series.Remove(seriesToRemove);
+                SetChartBoundaries();
+            }
+        }
+
+        private void HandleMetrics(Metric metric)
+        {
+            if (!string.IsNullOrEmpty(metric.Series) &&
+                _seriesIndex.ContainsKey(metric.Series))
+            {
+                var series = _seriesIndex[metric.Series];
+                series.Points.AddXY(xPosCounter++, metric.CounterValue);
+                while (series.Points.Count > MaxPoints) series.Points.RemoveAt(0);
+                SetChartBoundaries();
+            }
         }
 
         #endregion
+
+        private void SetChartBoundaries()
+        {
+            double maxAxisX, maxAxisY, minAxisX, minAxisY = 0.0d;
+            var allPoints = _seriesIndex.Values.SelectMany(series => series.Points).ToList();
+            var yValues = allPoints.SelectMany(point => point.YValues).ToList();
+            maxAxisX = xPosCounter;
+            minAxisX = xPosCounter - MaxPoints;
+            maxAxisY = yValues.Count > 0 ? Math.Ceiling(yValues.Max()) : 1.0d;
+            minAxisY = yValues.Count > 0 ? Math.Floor(yValues.Min()) : 0.0d;
+            if (allPoints.Count > 2)
+            {
+                var area = _chart.ChartAreas[0];
+                area.AxisX.Minimum = minAxisX;
+                area.AxisX.Maximum = maxAxisX;
+                area.AxisY.Minimum = minAxisY;
+                area.AxisY.Maximum = maxAxisY;
+            }
+        }
     }
 }
---
+++ src/Unit-2/DoThis/Actors/ChartingMessages.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace ChartApp.Actors
+{
+    public class GatherMetrics { }
+
+    public class Metric
+    {
+        public string Series { get; }
+        public float CounterValue { get;  }
+
+        public Metric(string series, float counterValue)
+        {
+            Series = series;
+            CounterValue = counterValue;
+        }
+    }
+
+    public enum CounterType
+    {
+        Cpu, Memory, Disk
+    }
+
+    public class SubscribeCounter
+    {
+        public CounterType Counter { get; }
+        public IActorRef Subscriber { get; }
+
+        public SubscribeCounter(CounterType counter, IActorRef subscriber)
+        {
+            Counter = counter;
+            Subscriber = subscriber;
+        }
+    }
+
+    public class UnsubscribeCounter
+    {
+        public CounterType Counter { get;}
+        public IActorRef Subscriber { get; }
+
+        public UnsubscribeCounter(CounterType counter, IActorRef subscriber)
+        {
+            Counter = counter;
+            Subscriber = subscriber;
+        }
+    }
+}
